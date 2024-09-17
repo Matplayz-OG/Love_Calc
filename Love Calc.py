7d0cbe2cd4bd8e50b610bb82d5ae71a3dd97296e
Ayush <ayushsmart60@gmail.com>
2024-09-17 20:54:29 +0530

    main.py and license uploaded
    These documents contains main file of love calc which contains whole src and the License

---
+++ Love Calc.py
@@ -0,0 +1,79 @@
+# konnichiwa watashi no keeds
+# watashi wa anata no otoosan desu
+# welcome to watashi no ai calc
+import random
+import time
+print("Welcome Darindo To your")
+time.sleep(1)
+print("(bhak BC it's of Our Master Ayush)")
+print("so ha kaha tha mei")
+time.sleep(1)
+print("ha")
+time.sleep(1)
+print('Love Calculator')
+time.sleep(1)
+while True:
+    a = input('Pehle Vyakti Ka Naam Btaya Jaae: ').capitalize() # prathan vyakti obviously
+    b = input('Dusre Vyakti Ka Naam Btaya Jaae: ').capitalize() # dusra vyakti
+    c = random.randint(69,101) #pratisat
+    d = random.randint(26,101)
+    e = random.randint(73,101)
+    f = random.randint(54,101)
+    g = random.randint(38,101)
+    h = random.randint(40,101)
+    i = random.randint(49,101)
+    j = random.randint(38,101)
+    k = random.randint(19,101)
+    l = random.randint(78,101)
+    tn = c + d + e + f + g + h + i + j + k + l
+    fp = tn/10
+#print(f"{fp}")
+    time.sleep(1)
+# Masters Section
+#    if a == 'Ayush' or 'Ayush gupta':
+#        print('Ram Ram Master')
+    if a == 'Ayush' or a == 'Ayush gupta':
+        print('Ram Ram Master')
+        if b == 'Mansi':
+            print(f'Love between {a} and {b} was always 0%')
+        elif b == 'Raina':
+            print(f'Love between {a} and {b} is 0% according to me baki mere bhai ke according {fp}%')
+        elif b == 'Akshita':
+            print(f"C'mon bro he loves her, but still you wanna find it right it's {fp}%")
+        else:
+            print(f"Love between {a} and {b} is {fp}%")
+# huihuihui master no son no section
+    elif a == 'Nikhil':
+        time.sleep(2)
+        print("Ram Ram Hamare Malik ke bete")
+        time.sleep(1)
+        if b == "Mansi":
+            print(f'Yaar tu {b} se pyaar krta h and currently tu usse {fp}% pyar krta h')
+        elif b == "Pranjal":
+            print(f'Bhai tera love {b} ke liye kbhi 90% se neeche ja hi nhi skta but still abhi tu usse {fp}% pyar krta h')
+        elif b == "Neha":
+            print(f'Yaar tu {b} se pyaar krta h and currently tu usse {fp}% pyar krta h')
+        else:
+            print(f"Love between {a} and {b} is {fp}%")
+# master no another son
+    elif a == 'Shubham':
+        time.sleep(1)
+        print("Ram Ram Hamare Malik ke Bhai")
+        time.sleep(1)
+        if b == "Mansi":
+            print(f'Yaar tu {b} se pyaar krta h and abhi tu usse {fp}% pyar krta h')
+        else:
+            print(f"Love between {a} and {b} is {fp}%")
+    else:
+        print(f"Love between {a} and {b} is {fp}%")
+
+    if a == "Nikhil" or b == "Nikhil" and fp <= 40:
+        print("Remember Chhote Maalik ke bete, Percentage can be false but not your heart")
+    elif a == 'Ayush' or a == 'Ayush Gupta':
+        print('Master, Percentage can be false but not your heart') 
+    else:
+        print('Bro, Percentage can be false but not your heart')      
+    repeat = input('Bhai Recheck krna h kya Y/N: ').lower()
+    if repeat != 'y':
+        print('Chal Bhai Bye')
+        break
